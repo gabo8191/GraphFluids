0ec3c6e6264834467ab97e2025cbfaf10350b58b
Gabriel Castillo <gabriel.castillo03@uptc.edu.co>
2023-09-06 17:10:46 -0500

    try the separate plots

--- main.py
+++ main.py
@@ -28,6 +28,10 @@ class DataApp:
         self.button_text = "#f8f8f2"
         self.highlight_color = "#66d9ef"
 
+        self.graph_color = "blue"  # Color predeterminado de la gráfica
+        self.x_label = "X"  # Etiqueta predeterminada para el eje X
+        self.y_label = "Y"  # Etiqueta predeterminada para el eje Y
+
         self.font = ('Courier New', 18)
 
         self.sidebar_visible = True
@@ -39,8 +43,6 @@ class DataApp:
         self.graph_color_label = tk.Label(
             self.graph_frame, text=f"Color de la Gráfica: {self.graph_color}", font=self.font)
 
-        self.x_label = "X"  # Etiqueta predeterminada para el eje X
-        self.y_label = "Y"  # Etiqueta predeterminada para el eje Y
         # Crear un menú de edición
         self.edit_menu = tk.Menu(self.root)
         self.root.config(menu=self.edit_menu)
@@ -56,46 +58,38 @@ class DataApp:
         # Opción para asignar nombres a los ejes
         self.edit_menu.add_command(
             label="Asignar Nombres a los Ejes", command=self.assign_axis_labels)
+        self.plot = None
         self.create_sidebar()
         self.create_data_section()
         self.create_graph_section()
 
-        self.root.mainloop()
-
     def change_graph_color(self):
-        # Función para cambiar el color de la gráfica
         if hasattr(self, 'data'):
-            # Solicitar al usuario que seleccione un nuevo color
-            # [1] para obtener el código hexadecimal del color
             new_color = colorchooser.askcolor()[1]
 
             if new_color:
-                # Actualizar el color de la gráfica y la etiqueta de color
                 self.graph_color = new_color
                 self.graph_color_label.config(
                     text=f"Color de la Gráfica: {new_color}")
 
-                # Volver a trazar la gráfica con el nuevo color
-                self.plot_graph()
+                # Llamar a la función para actualizar la gráfica
+                self.update_graph()
 
     def assign_axis_labels(self):
-        # Función para asignar nombres a los ejes
         if hasattr(self, 'data'):
-            # Solicitar al usuario que ingrese nombres para los ejes
             x_label = simpledialog.askstring(
                 "Asignar Eje X", "Ingrese un nombre para el Eje X:")
             y_label = simpledialog.askstring(
                 "Asignar Eje Y", "Ingrese un nombre para el Eje Y:")
 
             if x_label and y_label:
-                # Actualizar las etiquetas de los ejes
                 self.x_label = x_label
                 self.y_label = y_label
                 self.axis_labels_label.config(
                     text=f"Eje X: {x_label}, Eje Y: {y_label}")
 
-                # Volver a trazar la gráfica con las nuevas etiquetas de los ejes
-                self.plot_graph()
+                # Llamar a la función para actualizar la gráfica
+                self.update_graph()
 
     def create_sidebar(self):
         self.sidebar_frame = tk.Frame(self.root, bg=self.bg_color, width=200)
@@ -178,6 +172,31 @@ class DataApp:
                 self.data_display.insert(
                     tk.END, f"Error al cargar los datos: {str(e)}")
 
+    def download_template(self):
+        template_path = "data/formato.txt"
+
+        file_path = filedialog.asksaveasfilename(
+            initialdir="/", title="Guardar como", filetypes=(("txt files", "*.txt"), ("all files", "*.*")))
+
+        if file_path:
+            try:
+                if not file_path.endswith(".txt"):
+                    file_path += ".txt"
+
+                with open(template_path, 'rb') as template_file:
+                    template_content = template_file.read()
+
+                with open(file_path, 'wb') as destination_file:
+                    destination_file.write(template_content)
+
+                self.data_display.delete("1.0", tk.END)
+                self.data_display.insert(
+                    tk.END, "Plantilla descargada correctamente.")
+            except Exception as e:
+                self.data_display.delete("1.0", tk.END)
+                self.data_display.insert(
+                    tk.END, f"Error al descargar la plantilla: {str(e)}")
+
     def update_table(self):
         self.data_table.delete(*self.data_table.get_children())
         for index, row in self.data.iterrows():
@@ -216,6 +235,40 @@ class DataApp:
             self.sidebar_frame.pack(fill='y', side='left')
         self.sidebar_visible = not self.sidebar_visible
 
+    def plot_graph(self):
+        if hasattr(self, 'data'):
+            if self.plot is not None:
+                # Actualiza los datos de la gráfica en lugar de crear una nueva figura
+                self.plot.get_tk_widget().destroy()
+
+            plt.figure(figsize=(6, 4))
+            plt.plot(self.data['X'], self.data['Y'],
+                     marker='o', color=self.graph_color)
+            plt.xlabel(self.x_label)
+            plt.ylabel(self.y_label)
+            plt.title('Gráfica de Datos')
+            plt.grid(True)
+
+            self.plot = FigureCanvasTkAgg(plt.gcf(), master=self.graph_canvas)
+            self.plot.draw()
+            self.plot.get_tk_widget().pack(fill=tk.BOTH, expand=True)
+
+    def update_graph(self):
+        # borrar la gráfica actual
+        self.graph_canvas.delete("all")
+        # crear una nueva gráfica
+        plt.figure(figsize=(6, 4))
+        plt.plot(self.data['X'], self.data['Y'],
+                 marker='o', color=self.graph_color)
+        plt.xlabel(self.x_label)
+        plt.ylabel(self.y_label)
+        plt.title('Gráfica de Datos')
+        plt.grid(True)
+        # actualizar la gráfica
+        self.plot = FigureCanvasTkAgg(plt.gcf(), master=self.graph_canvas)
+        self.plot.draw()
+        self.plot.get_tk_widget().pack(fill=tk.BOTH, expand=True)
+
     def switch_view(self, view):
         if view == self.current_view:
             return
@@ -233,47 +286,6 @@ class DataApp:
         self.current_view = view
         if self.sidebar_visible:
             self.sidebar_frame.pack(fill='y', side='left')
-
-    def plot_graph(self):
-        if hasattr(self, 'data'):
-            self.graph_canvas.delete("all")
-
-            plt.figure(figsize=(6, 4))
-            plt.plot(self.data['X'], self.data['Y'],
-                     marker='o', color=self.graph_color)
-            plt.xlabel(self.x_label)
-            plt.ylabel(self.y_label)
-            plt.title('Gráfica de Datos')
-            plt.grid(True)
-
-            self.plot = FigureCanvasTkAgg(plt.gcf(), master=self.graph_canvas)
-            self.plot.draw()
-            self.plot.get_tk_widget().pack(fill=tk.BOTH, expand=True)
-
-    def download_template(self):
-        template_path = "data/formato.txt"
-
-        file_path = filedialog.asksaveasfilename(
-            initialdir="/", title="Guardar como", filetypes=(("txt files", "*.txt"), ("all files", "*.*")))
-
-        if file_path:
-            try:
-                if not file_path.endswith(".txt"):
-                    file_path += ".txt"
-
-                with open(template_path, 'rb') as template_file:
-                    template_content = template_file.read()
-
-                with open(file_path, 'wb') as destination_file:
-                    destination_file.write(template_content)
-
-                self.data_display.delete("1.0", tk.END)
-                self.data_display.insert(
-                    tk.END, "Plantilla descargada correctamente.")
-            except Exception as e:
-                self.data_display.delete("1.0", tk.END)
-                self.data_display.insert(
-                    tk.END, f"Error al descargar la plantilla: {str(e)}")
 
 
 if __name__ == "__main__":
